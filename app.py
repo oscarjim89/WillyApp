3f8d9b71cb9531c2f5ccb92124c29319a497c5bb
oscarjim89 <oscar.jim.89@gmail.com>
2020-03-28 10:11:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,14 +80,14 @@ def rotate():
 
 @app.route('/rotateleft', methods=['POST'])
 def rotateleft():
-    W.rotatebyTime(direction=left)
+    W.rotatebyTime(direction=left,seconds=None)
     now = datetime.now()
     response = now.strftime("%H:%M:%S: ")+"Rotating left...<BR>"
     return jsonify({'data': response})
 
 @app.route('/rotateright', methods=['POST'])
 def rotateright():
-    W.rotatebyTime(direction=right)
+    W.rotatebyTime(direction=right,seconds=None)
     now = datetime.now()
     response = now.strftime("%H:%M:%S: ")+"Rotating right...<BR>"
     return jsonify({'data': response})

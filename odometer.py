72c6d957522ab76f33f40b736a62918ae007d75f
oscarjim89 <oscar.jim.89@gmail.com>
2020-04-10 20:14:39 +0200

    Update odometer.py

--- odometer.py
+++ odometer.py
@@ -2,19 +2,19 @@ import struct, os
 
 class odometer():
     def __init__(self):
-        self.__file = open("/dev/input/mice", "rb");
+        self.__file = open("/dev/input/mice", "rb")
         self.__x_ini = 0
         self.__y_ini = 0
         self.__x_fi = 0
         self.__y_fi = 0
 
     def start(self):
-        buf = self.__file.read(3); 
-        self.__x_ini,self.__y_ini = struct.unpack( "bb", buf[1:] )
+        buf = self.__file.read(3)
+        self.__x_ini,self.__y_ini = struct.unpack( "dd", buf[1:] )
 
     def stop(self):
         buf = self.__file.read(3)
-        self.__x_fi,self.__y_fi = struct.unpack("bb", buf[1:] )
+        self.__x_fi,self.__y_fi = struct.unpack("dd", buf[1:] )
         x = self.__x_fi - self.__x_ini
         y = self.__y_fi - self.__y_ini
         return x, y

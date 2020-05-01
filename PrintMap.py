b8d115cb83f843e8f9e6d0ada61d49705e80c079
oscarjim89 <oscar.jim.89@gmail.com>
2020-05-01 21:37:33 +0200

    Update PrintMap.py

--- PrintMap.py
+++ PrintMap.py
@@ -13,21 +13,21 @@ class PrintMap():
 
     def printSVG(self):
 
-        xi = 50
-        yi = 50
+        x1 = 50
+        y1 = 50
 
         image = "<!DOCTYPE html><html><div style=\"height: 500px;width: 500px;\"><svg height=\"100%\" width=\"100%\">"
 
         for i in range(self.__count):
-            x1 = xi + (self.__journal["punts"][i]["pos"]["x"]*self.__res)
-            y1 = yi - (self.__journal["punts"][i]["pos"]["y"]*self.__res)
+            x1 = x1 + (self.__journal["punts"][i]["pos"]["x"]*self.__res)
+            y1 = y1 - (self.__journal["punts"][i]["pos"]["y"]*self.__res)
 
             try:
-                x2 = xi + (self.__journal["punts"][i+1]["pos"]["x"]*self.__res)
-                y2 = yi - (self.__journal["punts"][i+1]["pos"]["y"]*self.__res)
+                x2 = x1 + (self.__journal["punts"][i+1]["pos"]["x"]*self.__res)
+                y2 = y1 - (self.__journal["punts"][i+1]["pos"]["y"]*self.__res)
             except IndexError:
-                x2 = xi + (self.__journal["punts"][0]["pos"]["x"]*self.__res)
-                y2 = yi + (self.__journal["punts"][0]["pos"]["y"]*self.__res)
+                x2 = x1 + (self.__journal["punts"][0]["pos"]["x"]*self.__res)
+                y2 = y1 + (self.__journal["punts"][0]["pos"]["y"]*self.__res)
 
             image = image + "<line x1=\""+str(x1)+"%\" y1=\""+str(y1)+"%\" x2=\""+str(x2)+"%\" y2=\""+str(y2)+"%\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />"            
 
